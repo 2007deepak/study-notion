117689947e95870eadcf8f0c2e0192a455454cb4
Deepak Kumar <dk4503835@gmail.com>
2025-06-24 04:47:54 +0530

    Added OTP files

---
+++ models/OTP.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const mailSender = require("../utills/mailSender");
+
+const  OTPSchema = new mongoose.Schema({
+
+    email:{
+        type: String,
+        reqiured: true,
+    },
+    otp:{
+        typr:String,
+        required: true
+    },
+    createdAt:{
+        type:DataTransfer,
+        default: Date.now,
+        expires: 5*60,
+    },
+
+    
+});
+
+// A Function -> to send emails
+
+async function sendVerificationEmail(email, otp){
+
+    try{
+        const mailResponse = await mailSender(email, "Verifcation Email, from studyNotion", otp);
+        console.log("Email sent successfully:", mailResponse);
+
+    }
+    catch(error){
+        console.log("Error occured while sending mail:", Error);
+        throw error;
+
+    }
+}
+// Otp generateing middleware
+// ye OTP Database me save hone se pehle chalega
+OTPSchema.pre("save", async function(next){
+    await sendVerificationEmail(this.email, this.otp);
+    next();
+})
+module.exports = mongoose.model("OTP", OTPSchema)
