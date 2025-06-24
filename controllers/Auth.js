7a98169e149f16b1f24719c4c32a59939ab52560
Deepak Kumar <dk4503835@gmail.com>
2025-06-24 18:40:10 +0530

    Added user login functionality with JWT token generation

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,6 +1,9 @@
 const User = require("../models/user.js");
 const OTP = require("../models/OTP.js");
 const otpGenerator = reqiure("otp-generator");
+const bcrypt = required("bcrypt");
+const jwt = required("jsonwebtoken");
+
 
 require("dotenv").config();
 
@@ -62,6 +65,10 @@ exports.sendOTP = async(req , res) =>{
         message: "Otp sent Successfully",
         otp,
       });
+
+      
+      
+
     }catch(error)
     {
         console.log(error);
@@ -72,10 +79,121 @@ exports.sendOTP = async(req , res) =>{
         
     }
 
-
 }
 
 //signup
+
+exports.signUp = async(req, res) => {
+
+   try{
+     //  data fetch  from request body
+     const {
+       email,
+       password,
+       otp,
+       firstName,
+       lastName,
+       confirmPassword,
+       accountType,
+       contactNumber,
+     } = req.body;
+
+     //validate karlo
+     if (
+       !firstName ||
+       !lastName ||
+       !email ||
+       !password ||
+       !confirmPassword ||
+       !otp
+     ) {
+       return res.status(400).json({
+         success: false,
+         message: "Please fill all the  fields are  required",
+       });
+     }
+
+     //check is user alread exist or not
+
+     const existingUser = await User.findOne({ email });
+
+     if (existingUser) {
+       return res.status(401).json({
+         success: false,
+         message: "User is already registered",
+       });
+     }
+
+     //password match or not
+     if (password !== confirmPassword) {
+       return res.status(400).json({
+         success: false,
+         message:
+           "Password and ConfirmPassword  Value does not match, please try again",
+       });
+     }
+
+     //find the otp Stored for the user
+
+     const recentOtp = await OTP.find({ email })
+       .sort({ createdAT: -1 })
+       .limit(1);
+
+     console.log(recentOtp);
+
+     //validate OTP
+
+     if (recentOtp.length === 0) {
+       //OTP not found
+       return res.status(404).json({
+         success: false,
+         message: "OTP not Found",
+       });
+     } else if (otp !== recentOtp[0].otp) {
+       //invalid OTP
+       return res.status(400).json({
+         success: false,
+         message: "OTP Not Matched Please try again",
+       });
+     }
+
+     //Hash Password
+     const hashPassword = await bcrypt.hash(password, 10);
+
+     // entry create in Database
+     const profileDetails = await Profiler.create({
+       gender: null,
+       dateOfBirth: null,
+       about: null,
+       contactNumber: null,
+     });
+
+     const user = await User.create({
+       firstName,
+       lastName,
+       email,
+       password: hashPassword,
+       accountType,
+       additionalDetails: profileDetails._id,
+       // image:
+     });
+     // return response
+
+     return res.status(500).json({
+       success: true,
+       message: "User is Registered Successfully",
+       user,
+     });
+   }
+   catch(error){
+    console.log(error);
+    return res.status(500).json({
+        success: false,
+        message: error.message,  
+      });
+    
+   }
+}
 
 
 //Login
