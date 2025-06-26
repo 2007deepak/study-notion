5c65aa665d3279b23d931e51c1e8f3a8b39e6f4e
Deepak Kumar <dk4503835@gmail.com>
2025-06-26 13:30:35 +0530

    create section

---
+++ controllers/Section.js
@@ -0,0 +1,62 @@
+    const Course = require("../models/Course.js");
+    const Section = require("../models/Section.js");
+
+
+
+    exports.createSection = async(req,res) => {
+
+
+    //fetch data
+
+    const {SectionName , courseId} = req. body;
+
+    // data validation
+    try{
+        
+    if(!SectionName || !courseId){
+
+        return res.status(400).json({
+            success:false,
+            message: "Missing Properties"
+        }) 
+
+    }
+    //create Section
+
+    const  newSection = await newSection.create({SectionName});
+
+
+    //update the course with section objectId
+
+    const course = await course.findOneAndUpdate(
+                                        courseId,
+                                        {
+                                        $push:{
+                                            courseContent:newSection._id,
+
+                                        } ,
+                                        },
+                                        {new:true},
+                                        
+    );
+
+    //HW : use populate to replace section / sub-section both in the updatedCourseDetalis
+
+    //return response
+    return res.status(200).json({
+        success:true,
+        message:"Section created successfully",
+        updatedCourseDetails,
+    });
+
+    }
+    catch(error){
+        console.log(error);
+        return res.stauts(200).json({
+            success: true,
+            message:"Unable to create Section,plese try again",
+
+        });
+    
+    }
+}
