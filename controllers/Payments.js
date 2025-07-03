74b6e9128f1b8e268472f5064b2ee74dbd6902ba
Deepak Kumar <dk4503835@gmail.com>
2025-07-03 07:30:51 +0530

    feat:(capturePayment):integrate

--- controllers/Payments.js
+++ controllers/Payments.js
@@ -13,7 +13,7 @@ const { findById } = require("../models/Profile");
 
 exports.capturePayment = async (req , res) => {
 
-    // get courseId and userId
+   // get courseId and userId
 
     const {course_id} = req.body;
     const userId = req.user.id;
@@ -62,7 +62,7 @@ exports.capturePayment = async (req , res) => {
         
     }
 
-//order create
+  //order create
 
   const amount = course.price;
   const currency = "INR";
@@ -103,4 +103,85 @@ exports.capturePayment = async (req , res) => {
         });
       }
 
-    };
+};
+
+    //Verify the Signatutre of Rozarpay and Server
+exports.verifySignature = async(req, res) => {
+
+  const webhookSecret = "12345678";
+  const signature =req.headers["x-razorpay-signature"];
+
+  // isame "sha256" ek algorigh hai and webhook hai
+  const shasum = crypto.createHmac("sha256",webhookSecret);
+  //convert into string formate 
+  shasum.upadte(JSON.stringify(req.body));
+  //jab bhi ham koi algo use karate hai use darsane ke liye ham digest ka use karte hai
+  const digest = shasum.digest("hex");
+
+  if(signature === digest)
+  {
+    
+   console.log("Payment is Authorised");
+        
+      
+    const {courseId,userId} = req.body.payload.payment.entity.notes;
+
+    try
+    {
+      //fulfil the action
+      //find the course and enroll the student in it
+      const enrolledCourse =await Course.findOneAndUpdate(
+                                      
+                                        {_id:courseId},
+                                        {$push:{studentsEnrolled : userId}},
+                                        {new: true},
+                                        
+      );
+        
+      if(!enrolledCourse){
+        return res.status(500).json({
+          success:false,
+          message:"Course not found"
+        });
+      }
+      console.log(enrolledCourse);
+
+      //find the student an added the course to their list enrolled courses me
+
+      const enrolledStudent = await User.findOneAndUpdate(
+                                      {_id:userId},
+                                      {$push:{courses:courseId}},
+                                      {new:true},
+      );
+      
+      console.log(enrolledStudent);
+      //mail send kardo confirmation wala
+      const emailResponse = await mailSender(isEnrolledStudent.email,
+        "Congaratulation from StudyNotion",
+        "Congratulation,you are onboarded into new CodeHelp Course",
+
+      );
+
+      console.log(emailResponse);
+      return res.status(200).json({
+        success : true,
+        message:"Singnature varified and Course Added",
+      });
+      }catch(error){
+          console.log(error);
+          return res.status(500).json({
+            success:false,
+            message:error.message,
+          })
+    }
+  }       
+  
+  else{
+    return res.status(400).json({
+      success:false,
+      message:"Invalid request"
+    });
+
+  }
+
+};
