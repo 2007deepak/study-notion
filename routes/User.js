e341d57404b615e5e7b462366d78a712175850fe
Deepak Kumar <dk4503835@gmail.com>
2025-07-17 16:18:26 +0530

    add router for login, Signup, and Authetication

--- routes/User.js
+++ routes/User.js
@@ -16,3 +16,24 @@ const {
 } = require("../controllers/ResetPassword")
 
 const {auth} = require("../middleware/auth")
+
+
+//Routes for login, Signup, and Authentication
+
+// ********************************************************************************************************
+//                                      Authentication routes
+// ********************************************************************************************************
+
+//Route for user login
+
+router.post("/login", login)
+
+//Route for user singup
+router.post("/signup",signUp)
+
+//Route for sending OTP to the user's email
+router.post("/sendOtp", sendOTP)
+
+//Route for Changing the Password
+router.post("/changepassword",auth, changePassword)
+
