05e8622bf95424304b7578b3def4a8d419fb68c2
Deepak Kumar <dk4503835@gmail.com>
2025-07-17 22:39:39 +0530

    feat: getEnrolledCourses and updateDisplayPicture

--- controllers/Profile.js
+++ controllers/Profile.js
@@ -1,6 +1,6 @@
 const Profile = require("../models/Profile.js");
 const User = require("../models/user.js");
-
+const { uploadImageToCloudinary } = require("../utils/imageUploader");
 
 exports.updateProfile = async (req, res) => {
 
@@ -107,7 +107,9 @@ exports.getALLUserDetails = async (res , req) => {
         // validation and  get user detalis 
         const userDetails = await User.findById(id).populate(
           "additionalDetails"
-        );
+        )
+        	.exec();
+		console.log(userDetails);
         //return response
         return res.status(200).json({
           success: true,
@@ -123,3 +125,59 @@ exports.getALLUserDetails = async (res , req) => {
         });
     }
 }
+
+
+exports.updateDisplayPicture = async (req, res) => {
+  try {
+    const displayPicture = req.files.displayPicture;
+    const userId = req.user.id;
+    const image = await uploadImageToCloudinary(
+      displayPicture,
+      process.env.FOLDER_NAME,
+      1000,
+      1000
+    );
+    console.log(image);
+    const updatedProfile = await User.findByIdAndUpdate(
+      { _id: userId },
+      { image: image.secure_url },
+      { new: true }
+    );
+    res.send({
+      success: true,
+      message: `Image Updated successfully`,
+      data: updatedProfile,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.getEnrolledCourses = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const userDetails = await User.findOne({
+      _id: userId,
+    })
+      .populate("courses")
+      .exec();
+    if (!userDetails) {
+      return res.status(400).json({
+        success: false,
+        message: `Could not find user with id: ${userDetails}`,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      data: userDetails.courses,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
