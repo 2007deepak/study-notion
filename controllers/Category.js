0e568b57077a82caf8e37c0eea50edb77a6cae22
Deepak Kumar <dk4503835@gmail.com>
2025-07-17 14:36:53 +0530

    feat: cotegoryPageDetails handler

--- controllers/Category.js
+++ controllers/Category.js
@@ -47,12 +47,12 @@ exports.createCategory = async (req , res) =>{
 exports.showAllCategory = async (req , res) => {
     try{
 
-        const allCategory = await Cotegory.find({} , {name:true , description: true});
+        const allCategory = await Category.find({} , {name:true , description: true});
 
         res.status(200).json({
-            success : false,
-            message:"All tags returned Successfull"
-        })
+          success: false,
+          data: allCategorys,
+        });
 
     }
     catch(error)
@@ -63,3 +63,52 @@ exports.showAllCategory = async (req , res) => {
         });
     }
 };
+
+//categoryPageDetails 
+
+exports.categoryPageDetails = async (req, res) => {
+    try {
+            //get categoryId
+            const {categoryId} = req.body;
+            //get courses for specified categoryId
+            const selectedCategory = await Category.findById(categoryId)
+              .populate("courses")
+              .exec();
+            //validation
+            if(!selectedCategory) {
+                return res.status(404).json({
+                    success:false,
+                    message:'Data Not Found',
+                });
+            }
+           
+            // mujhe aisi category ki id nikal kar do jo is cotegory id me nhi hai matalab category idid ko chodkar
+            //baki sb mil jayengi
+        
+            const differentCategories = await Category.find({
+              _id: { $ne: categoryId },
+            })
+              .populate("courses")
+              .exec();
+
+            //get top 10 selling courses
+            //HW - write it on your own
+
+            //return response
+            return res.status(200).json({
+                success:true,
+                data: {
+                    selectedCategory,
+                    differentCategories,
+                },
+            });
+
+    }
+    catch(error ) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+}
