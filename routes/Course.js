74c6227206016369d09a18865d28beee269cba66
Deepak Kumar <dk4503835@gmail.com>
2025-07-18 16:47:35 +0530

    feat: Added Middleware and course Routes

--- routes/Course.js
+++ routes/Course.js
@@ -7,12 +7,50 @@ const router =  express.Router()
 const {createCourse, getCourseDetails, getAllCourse} = require("../controllers/Course");
 
 
-//Categories Conrollers Imports
+//Categories Controllers Imports
 const{createCategory, showAllCategory, categoryPageDetails} = require("../controllers/Category");
 
-//Section Conroller imports
-const{createSection,updateSectioon,deleteSection} = require("../controllers/Section")
+//Section Controller imports
+const{createSection,updateSection,deleteSection} = require("../controllers/Section")
 
-//Subsection Conroller imports
+//Subsection Controller imports
 
 const{createSubSection, updateSubSection, deleteSubSection} = require("../controllers/Subsection")
+
+//const controllers Rating and Reviwe imports
+const {createRating, getAverageRating,getAllRating} = require("../controllers/RatingAndReview")
+
+//import middleware 
+const {auth,isStudent,isInstructor,isAdmin} = require("../middleware/auth");
+
+
+// ********************************************************************************************************
+//                                      Course routes
+// ********************************************************************************************************
+
+//Course can Only be Created by Instructors
+router.post("/createCourse", auth, isInstructor, createCourse);
+
+//Add a Section to a Course
+router.post("/addSection", auth, isInstructor, createSection);
+
+//Add update a Section 
+router.post("/updateSection", auth, isInstructor, updateSection);
+
+//Delete a Section
+router.post("/deleteSection", auth, isInstructor, deleteSection);
+
+// Edit Sub Section
+router.post("/updateSubSection", auth, isInstructor, updateSubSection);
+
+//delete Sub Section
+router.post("/deleteSubSection", auth, isInstructor, deleteSubSection);
+
+// Add a Sub Section to a Section
+router.post("/addSubSection", auth, isInstructor, createSubSection);
+
+//Get all Registered Course
+router.get("/getAllCourse", getAllCourse)
+
+//get Details for a Specific Courses
+router.get("/getCourseDetalis", getCourseDetails);
