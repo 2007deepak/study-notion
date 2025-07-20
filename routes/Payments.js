09153db0923701a241ea7974f6442ca2aef55828
Deepak Kumar <dk4503835@gmail.com>
2025-07-20 19:35:18 +0530

    Some addition changes

--- routes/Payments.js
+++ routes/Payments.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const router =  express.Router()
 
-const {capturePayment,verifySignature } = require("../controllers/Payments")
+const {capturePayment,verifySignature } = require("../controllers/Payments.js")
 
-const {auth,isInstructor, isStudent,isAdmin } = require("../middleware/auth")
+const {auth,isInstructor, isStudent,isAdmin } = require("../middleware/auth.js")
 
 
-router.post("/cpaturePaymnet", auth, isStudent, capturePayment);
+router.post("/capturePayment", auth, isStudent, capturePayment);
 router.post("/verifySignature", verifySignature);
 
 module.exports = router
