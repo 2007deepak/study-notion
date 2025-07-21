20c90bba6562a06d7c07296dbfc95a63a74f29f1
Deepak Kumar <dk4503835@gmail.com>
2025-07-21 23:21:31 +0530

    Moved reset password routes to Profile route file and re-added ResetPassword controller imports

--- routes/Profile.js
+++ routes/Profile.js
@@ -10,6 +10,12 @@ const {
 } = require("../controllers/Profile.js");
 
 
+const {
+    resetPasswordToken,
+    resetPassword,
+} = require("../controllers/ResetPassword.js")
+
+
 // ********************************************************************************************************
 //                                      Profile routes
 // ********************************************************************************************************
@@ -22,5 +28,12 @@ router.get("/getuserDetails", auth, getALLUserDetails)
 //get Enrollment Course
 router.get("/getEnrolledCourse",auth,getEnrolledCourses)
 router.put("/updateDisplayPicture", auth, updateDisplayPicture)
+
+// Route for generating a reset password token
+router.post("/reset-password-token",resetPasswordToken);
+
+// Route for resetting user's password after verification
+router.post("/reset-password", resetPassword);
+
 
 module.exports = router;
