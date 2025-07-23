65561f80511c490253a3d57c2168d3e9797598ba
Deepak Kumar <dk4503835@gmail.com>
2025-07-23 18:00:33 +0530

    feat: add required validation to fields in subSectionSchema

--- models/SubSection.js
+++ models/SubSection.js
@@ -1,27 +1,20 @@
 const mongoose = require("mongoose");
 const subSectionSchema = new mongoose.Schema({
-
-
-    title : {
-        type : String,
-        required : true,
-
-    },
-    timeDuration : {
-
-        type: String,
-
-    },
-    description: {
-        type:String,
-
-    },
-    videoUrl:{
-        type:String,
-    },
-
-
-
+  title: {
+    type: String,
+    required: true,
+  },
+  timeDuration: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  videoFile: {
+    type: String,
+       required: true,
+  },
 });
 
 module.exports = mongoose.model("SubSection", subSectionSchema);
