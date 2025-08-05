6a15bcd922c57490a3517cccdfc64257a2505f8f
Deepak Kumar <dk4503835@gmail.com>
2025-08-05 23:44:25 +0530

    Created profileSlice to manage user profile state using Redux Toolkit

---
+++ Frontend/src/slices/profileSlice.jsx
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+ user:null,
+};
+const profileSlice = createSlice({
+  name: "Profile",
+  initialState: initialState,
+  reducers: {
+    setToken(state, value) {
+      state.user = value.payload;
+    },
+  },
+});
+
+export const { setUser } = profileSlice.actions;
+export default profileSlice.reducer;
