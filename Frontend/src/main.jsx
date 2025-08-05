5eacf1300b383fe706cf180506a83f8cb18ef219
Deepak Kumar <dk4503835@gmail.com>
2025-08-05 23:53:50 +0530

    import provider,rootReducer and ConfigureStore inmain.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,10 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from "./reducer"
+import {Provider} from  "react-redux"
+
+const store = configureStore({
+  reducer:rootReducer,
+});
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    
+  <Provider store = {store}>
+    <BrowserRouter>
       <App />
-  </BrowserRouter>
+    </BrowserRouter>
+  </Provider>
 );
