4ced5b205f5ea364f530d2e30cdb6026bf3c85b5
Deepak Kumar <dk4503835@gmail.com>
2025-08-06 21:48:47 +0530

    feat: import Toster from react-hot-toast

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from "./reducer"
 import {Provider} from  "react-redux"
+import {Toaster} from "react-hot-toast"
 
 const store = configureStore({
   reducer:rootReducer,
@@ -15,6 +16,8 @@ createRoot(document.getElementById("root")).render(
   <Provider store = {store}>
     <BrowserRouter>
       <App />
+      <Toaster/>
     </BrowserRouter>
+    
   </Provider>
 );
