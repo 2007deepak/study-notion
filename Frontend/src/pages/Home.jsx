00341de8cc11d767b2d011abede711db440a788a
Deepak Kumar <dk4503835@gmail.com>
2025-08-13 22:29:45 +0530

    feat: import footer components

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -9,6 +9,8 @@ import TimelineSection from "../components/core/HomePage/TimelineSection.jsx"
 import LearningLanguageSection  from "../components/core/HomePage/LearningLanguageSection.jsx"
 import InstructorSection from "../components/core/HomePage/InstructorSection.jsx"
 import ExploreMore from "../components/core/HomePage/ExploreMore.jsx";
+import Footer from "../components/common/Footer";
+
 function Home (){
   return (
     <div>
@@ -34,7 +36,7 @@ function Home (){
           <HighlightText text={"Coding Skills"} />
         </div>
 
-        <div className=" mt-4 text-center text-lg font-bold text-richblack-300 max-w-[1000px]">
+        <div className="mt-3  sm:mt-4 text-base sm:text-lg md:text-xl font-bold text-richblack-300 max-w-[100%] sm:max-w-[1000px] mx-auto">
           With our online coding course, you can learn at your own pace, from
           anywhre in the world, get access to a wealth of resources,including
           hands-on projects,quizzes,and presonalized feedback from instructors.
@@ -50,16 +52,19 @@ function Home (){
           </CTAButton>
         </div>
 
-        <div className=" mx-3 my-12 shadow-blue-200 w-full max-w-[1000px]  ">
-          <video muted loop autoPlay>
-            <source src={Banner} type="video/mp4" />
-          </video>
+        <div className=" relative mx-3 my-12 shadow-blue-200 w-full max-w-[1000px]">
+          <div className="relative z-10  overflow-hidden">
+            <video muted loop autoPlay>
+              <source src={Banner} type="video/mp4" />
+            </video>
+          </div>
+          <div className="absolute top-4 left-4 w-full h-full  bg-white z-0"></div>
         </div>
 
         {/*Code Section 1 */}
-        <div className=" max-w-[1000px]">
+        <div className=" max-w-[1100px]">
           <CodeBlocks
-            position={"lg: flex-row"}
+            position={" flex md:flex-row flex-col sm:flex-row  "}
             heading={
               <div className="text-4xl font-semibold">
                 Unlock Your
@@ -82,11 +87,12 @@ function Home (){
             }}
             codeblock={`<<!DOCTYPE html>\nhead><title>Example</title><linkrel="stylesheet"href="styles.css"\nhead>\nbody\nh1><aheref="/">Header</a>\n</h1>`}
             codeColor={"text-yellow-25"}
+            gradient="from-[#8B5CF6]/40 via-[#EC4899]/40 to-[#FCD34D]/30"
           ></CodeBlocks>
         </div>
 
         {/*Code Section 2 */}
-        <div className=" max-w-[1000px]">
+        <div className="max-w-[1100px] w-full px-4 mx-auto ">
           <CodeBlocks
             position={"lg: flex-row-reverse"}
             heading={
@@ -110,6 +116,7 @@ function Home (){
             }}
             codeblock={`<<!DOCTYPE html>\nhead><title>Example</title><linkrel="stylesheet"href="styles.css"\nhead>\nbody\nh1><aheref="/">Header</a>\n</h1>`}
             codeColor={"text-yellow-25"}
+            gradient="from-[#22D3EE]/40 via-[#6366F1]/40 to-[#A855F7]/30"
           ></CodeBlocks>
         </div>
 
@@ -123,7 +130,7 @@ function Home (){
             <div className="h-[150px]"></div>
             <div className="flex flex-row gap-7 text-white">
               <CTAButton active={true} linkto={"/signup"}>
-                <div>
+                <div className=" flex flex-row items-center gap-2">
                   Explore Full Catalog
                   <FaArrowRight />
                 </div>
@@ -136,20 +143,22 @@ function Home (){
         </div>
 
         <div className="mx-auto w-11/12 max-w-maxContent flex flex-col justify-between ">
-          <div className="flex flex-row gap-5 mt-[95px] mb-10">
-            <div className="text-4xl font-semibold w-[45%]">
+          <div className="flex flex-col md:flex-row gap-8 md:gap-5  mt-[60px] md:mt-[95px] mb-10">
+            <div className=" text-3xl md:text-4xl font-semibold md:w-[45%] w-full md:text-left">
               Get the Skill you need for a
               <HighlightText text={"job that is in demand"} />
             </div>
 
-            <div className="flex flex-col gap-10 w-[40%] items-start">
+            <div className="flex flex-col  gap-6 md:gap-10 md:full md:w-[40%] md:items-start items-center md:text-left">
               <div className="text-[16px]">
                 This modern StudyNotion is the ditects its own.Today, to be a
                 competetive specialist requires more than professional skills.
               </div>
-              <CTAButton active={true} linkto={"/signup"}>
-                <div>Learn more</div>
-              </CTAButton>
+              <div className="w-full flex justify-start md:justify-start">
+                <CTAButton active={true} linkto={"/signup"}>
+                  <div className="font-semibold">Learn more</div>
+                </CTAButton>
+              </div>
             </div>
           </div>
           <TimelineSection />
@@ -167,6 +176,7 @@ function Home (){
         {/* Review Slider here */}
       </div>
       {/*Footer */}
+      <Footer />
     </div>
   );
 }
