b7225c2e0d4f08df2070b7ba0e6dfc9b43578af3
Deepak Kumar <dk4503835@gmail.com>
2025-08-14 23:10:42 +0530

    feat(api): add authentication endpoints in apis.js

--- Frontend/src/services/apis.js
+++ Frontend/src/services/apis.js
@@ -1,5 +1,15 @@
+{/* link mention kar diya ki kaun si link pe call karane jana hai */}
+
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const categogories = {
   CATEGORIES_API: BASE_URL + "/course/showAllCategory",
+};
+
+export const endpoints = {
+  SENDOTP_API: BASE_URL + "/auth/sendotp",
+  SIGNUP_API: BASE_URL + "/auth/signup",
+  LOGIN_API: BASE_URL + "/auth/login",
+  RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
+  RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
 };
