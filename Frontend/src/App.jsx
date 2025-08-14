d8eff1b8c98470f7a8265f9ed3d6d0fccee189a2
Deepak Kumar <dk4503835@gmail.com>
2025-08-14 23:08:30 +0530

    feat(routes): add login, signup, contact, and about page routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,13 @@
 
 import './App.css'
 import { Route,Routes } from 'react-router-dom';
-import Home from "./pages/Home.jsx"
-import Navbar from "./components/common/Navbar.jsx"
+import Home from "./pages/Home.jsx";
+import Navbar from "./components/common/Navbar.jsx";
+import Login  from "./pages/Login.jsx";
+import Singup from "./pages/Signup.jsx";
+import Contact from "./pages/Contact.jsx";
+import About from "./pages/About.jsx";
+
 function App() {
 
 
@@ -11,6 +16,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/singup" element={<Singup />} /> 
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
